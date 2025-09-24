c8fab8944f2cdaf21c7863e56bf9be63d859a2f2
Patrick Evans <holysoles97@gmail.com>
2025-09-24 11:52:08 -0500

    update formatter timeout for pin-github-action

--- init.lua
+++ init.lua
@@ -774,7 +774,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 1000,
             lsp_format = 'fallback',
           }
         end
